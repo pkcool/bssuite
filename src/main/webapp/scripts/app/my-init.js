33657ecab339795d0158f85738100dd8f5b902fe
pkcool <smaxllimit@gmail.com>
2015-10-29 12:43:28 +1100

    Added my-style, my-init script and pace lib

---
+++ src/main/webapp/scripts/app/my-init.js
@@ -0,0 +1,11 @@
+/**
+ * Created by smaxll on 25/10/2015.
+ */
+
+// Configuration for pace script
+paceOptions = {
+
+    // Only show the progress on regular and ajax-y page navigation,
+    // not every request
+    restartOnRequestAfter: false
+}
--- src/test/javascript/karma.conf.js
+++ src/test/javascript/karma.conf.js
@@ -27,6 +27,7 @@ module.exports = function (config) {
             'main/webapp/bower_components/json3/lib/json3.js',
             'main/webapp/bower_components/ng-file-upload/ng-file-upload.js',
             'main/webapp/bower_components/ngInfiniteScroll/build/ng-infinite-scroll.js',
+            'main/webapp/bower_components/PACE/pace.js',
             'main/webapp/bower_components/angular-mocks/angular-mocks.js',
             // endbower
             'main/webapp/scripts/app/app.js',
