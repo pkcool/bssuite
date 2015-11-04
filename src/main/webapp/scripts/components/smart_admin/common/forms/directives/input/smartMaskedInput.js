9340de92b00a50d8d4988a501a6dbb1afb95b40e
smaxll <smaxllimit@gmail.com>
2015-11-05 02:03:27 +1100

    Added masked input directive

---
+++ src/main/webapp/scripts/components/smart_admin/common/forms/directives/input/smartMaskedInput.js
@@ -0,0 +1,17 @@
+"use strict";
+
+angular.module('bssuiteApp').directive('smartMaskedInput', function(lazyScript){
+    return {
+        restrict: 'A',
+        compile: function(tElement, tAttributes){
+            tElement.removeAttr('smart-masked-input data-smart-masked-input');
+
+        	lazyScript.register('jquery-maskedinput').then(function(){
+
+	            var options = {};
+	            if(tAttributes.maskPlaceholder) options.placeholder =  tAttributes.maskPlaceholder;
+	            tElement.mask(tAttributes.smartMaskedInput, options);
+        	})
+        }
+    }
+});
