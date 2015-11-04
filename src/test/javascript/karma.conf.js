5117a7ae813aa1ebea29ab973ba6400c619a6c3c
smaxll <smaxllimit@gmail.com>
2015-11-05 02:06:27 +1100

    Fixes #3

--- src/test/javascript/karma.conf.js
+++ src/test/javascript/karma.conf.js
@@ -34,6 +34,7 @@ module.exports = function (config) {
             'main/webapp/bower_components/jquery-validation/dist/jquery.validate.js',
             'main/webapp/bower_components/jquery.maskedinput/dist/jquery.maskedinput.js',
             'main/webapp/bower_components/select2/dist/js/select2.js',
+            'main/webapp/bower_components/lodash/lodash.js',
             'main/webapp/bower_components/angular-mocks/angular-mocks.js',
             // endbower
             'main/webapp/scripts/app/app.js',
