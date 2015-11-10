f5d131f0820c980ae2b4981e2244352a9fe253ab
pkcool <smaxllimit@gmail.com>
2015-11-11 09:57:01 +1100

    Added a shell script to generate entities when needed

---
+++ src/main/resources/jp-entity-generator.sh
@@ -0,0 +1,36 @@
+yo jhipster:entity Store
+yo jhipster:entity Address
+yo jhipster:entity Contact
+yo jhipster:entity SupplierCategory
+yo jhipster:entity Supplier
+yo jhipster:entity Staff
+yo jhipster:entity CustomerCategory
+yo jhipster:entity Customer
+yo jhipster:entity PriceScale
+yo jhipster:entity StockFamily
+yo jhipster:entity TaxTable
+yo jhipster:entity StockGroup
+yo jhipster:entity Product
+yo jhipster:entity Carrier
+yo jhipster:entity Promotion
+yo jhipster:entity SalesOrder
+yo jhipster:entity SalesOrderLineItem
+yo jhipster:entity CustomerDiscountRule
+yo jhipster:entity SupplierDiscountRule
+yo jhipster:entity BackOrderLineItem
+yo jhipster:entity PurchaseOrder
+yo jhipster:entity PurchaseOrderLineItem
+yo jhipster:entity Invoice
+yo jhipster:entity InvoiceLineItem
+yo jhipster:entity Quote
+yo jhipster:entity QuoteLineItem
+yo jhipster:entity ProductRelationCategory
+yo jhipster:entity RelatedProduct
+yo jhipster:entity TaxActivityAudit
+yo jhipster:entity GoodsReceivedAudit
+yo jhipster:entity CalendarItem
+yo jhipster:entity ProductActivityAudit
+yo jhipster:entity Bookmark
+
+
+
